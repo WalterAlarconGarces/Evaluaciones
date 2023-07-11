eb1879b2d32661a82bce304f130c3a75d3bb0215
WalterAlarconGarces <101961998+WalterAlarconGarces@users.noreply.github.com>
2023-07-11 22:38:29 +0000

    implementación de la estructura de archivos y habilitación de la opción de crear archivo html

--- evaluacion4/file1.rb
+++ evaluacion4/file1.rb
@@ -40,12 +40,52 @@ def buid_web_page(hash_entrada)
 </div>
     "
   }
-  puts tarjetas
+  return tarjetas
 end
 
+codigo_html_primero = '<!DOCTYPE html>
+<html lang="es">
 
-buid_web_page(request(Enlace))
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Desafio NASA">
+    <link rel="shortcut icon" href="/assets/img/R.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&family=Lobster&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<title>NASA</title>
+</head>
 
+<body>
+    <div class="cointainer">
+        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-4">'
 
+codigo_html_segundo = '        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+        integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+        integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="/assets/js/script.js"></script>
+</body>
+
+</html>'
+
+web_completa = codigo_html_primero + buid_web_page(request(Enlace)) + codigo_html_segundo
+
+File.write("index.html",web_completa)
 
 
